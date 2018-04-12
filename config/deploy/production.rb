781565a2533a9a894c103d23f0ae55ff03d62b0e
SinnerAiZ <1036570699@qq.com>
2018-04-12 20:22:09 +0800

    修改部署地址

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-server 'mxjyu.com', user: 'ubuntu', roles: %w{app db web}
+server 'mxjyu.cn', user: 'ubuntu', roles: %w{app db web}
 set :rails_env, "production"
 set :stage, :pro
 
@@ -45,7 +45,7 @@ set :stage, :pro
 set :ssh_options, {
   keys: %w(/home/ubuntu/.ssh/id_rsa),
   forward_agent: false,
-  password: '1124520.'
+  password: 'mida1124'
 }
 #
 # The server-based syntax can be used to override options:
