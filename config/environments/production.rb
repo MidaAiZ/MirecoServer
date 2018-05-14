c5748cd9860c055a156c072944de635e487d7ace
Mida <1036570699@qq.com>
2018-05-14 20:05:30 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost'
+  host = '127.0.0.1'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
   address:              'smtp.163.com',
